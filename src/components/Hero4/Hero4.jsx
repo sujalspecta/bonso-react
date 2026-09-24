63b10e6ea36854424cee3b1755d642b6b7375469
sujalspecta <sujalspectaclicks@outlook.com>
2026-09-24 12:09:06 +0530

    swiper package update

--- src/components/Hero4/Hero4.jsx
+++ src/components/Hero4/Hero4.jsx
@@ -1,71 +1,91 @@
-import { Navigation, A11y } from "swiper/modules";
+import React from "react";
+import { Navigation, A11y, Parallax } from "swiper/modules"; // Added Parallax module
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Link } from 'react-router-dom'
-import hero3 from '../../images/slider/slide-13.jpg'
-import hero4 from '../../images/slider/slide-4.jpg'
+import 'swiper/css/parallax'; // Added Parallax style architecture
+import { Link } from 'react-router-dom';
+import hero3 from '../../images/slider/slide-13.jpg';
+import hero4 from '../../images/slider/slide-4.jpg';
 
 const ClickHandler = () => {
     window.scrollTo(10, 0);
-}
+};
 
 const Hero4 = () => {
-
     return (
         <section className="wpo-hero-slider wpo-hero-style-2">
             <Swiper
-
-                modules={[Navigation, A11y]}
+                // Installed modules including Parallax
+                modules={[Navigation, A11y, Parallax]}
                 spaceBetween={0}
                 slidesPerView={1}
                 loop={true}
                 speed={1800}
                 parallax={true}
-                navigation
+                navigation={true}
+                
+                /* ANTI-COLLAPSE ARCHITECTURE & DOM OBSERVERS */
+                observer={true}
+                observeParents={true}
+                style={{ width: "100%", height: "100vh" }} 
+                onSwiper={(swiper) => {
+                    setTimeout(() => {
+                        if (swiper && !swiper.destroyed) {
+                            swiper.update();
+                        }
+                    }, 150);
+                }}
             >
-                <SwiperSlide>
-                    <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero3})` }}>
+                {/* Slide 1 */}
+                <SwiperSlide style={{ width: "100%", height: "100%" }}>
+                    <div 
+                        className="slide-inner slide-bg-image" 
+                        style={{ backgroundImage: `url(${hero3})`, height: "100%", width: "100%" }}
+                    >
                         <div className="container-fluid">
                             <div className="slide-content">
-                                <div className="slide-title">
+                                {/* Added staggered parallax layout values */}
+                                <div className="slide-title" data-swiper-parallax="-400">
                                     <h2>We <span>build</span> Your Dream Best House</h2>
                                 </div>
-                                <div className="slide-title-sub">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, justo,
-                                        habitant.</p>
+                                <div className="slide-title-sub" data-swiper-parallax="-300">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, justo, habitant.</p>
                                 </div>
                                 <div className="clearfix"></div>
-                                <div className="slide-btns">
+                                <div className="slide-btns" data-swiper-parallax="-200">
                                     <Link onClick={ClickHandler} to="/about" className="theme-btn">Get Started</Link>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </SwiperSlide>
-                <SwiperSlide>
-                    <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero4})` }}>
+
+                {/* Slide 2 */}
+                <SwiperSlide style={{ width: "100%", height: "100%" }}>
+                    <div 
+                        className="slide-inner slide-bg-image" 
+                        style={{ backgroundImage: `url(${hero4})`, height: "100%", width: "100%" }}
+                    >
                         <div className="container-fluid">
                             <div className="slide-content">
-                                <div className="slide-title">
+                                <div className="slide-title" data-swiper-parallax="-400">
                                     <h2>We <span>Ensure</span> Quality Industrial Solution.</h2>
                                 </div>
-                                <div className="slide-title-sub">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, justo,
-                                        habitant.</p>
+                                <div className="slide-title-sub" data-swiper-parallax="-300">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, justo, habitant.</p>
                                 </div>
                                 <div className="clearfix"></div>
-                                <div className="slide-btns">
+                                <div className="slide-btns" data-swiper-parallax="-200">
                                     <Link onClick={ClickHandler} to="/about" className="theme-btn">Get Started</Link>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
-                ...
             </Swiper>
         </section>
-    )
-}
+    );
+};
+
 export default Hero4;
--- src/components/hero2/Hero2.jsx
+++ src/components/hero2/Hero2.jsx
@@ -1,73 +1,89 @@
-import { Navigation, A11y } from "swiper/modules";
+import React from "react";
+import { Navigation, A11y, Parallax } from "swiper/modules"; 
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Link } from 'react-router-dom'
-import hero1 from '../../images/slider/slide-12.jpg'
-import hero2 from '../../images/slider/slide-11.jpg'
+import 'swiper/css/parallax'; // Added Parallax styling sheet for smooth movement
+import { Link } from 'react-router-dom';
+import hero1 from '../../images/slider/slide-12.jpg';
+import hero2 from '../../images/slider/slide-11.jpg';
 
 const ClickHandler = () => {
     window.scrollTo(10, 0);
-}
+};
+
 const Hero = () => {
     return (
-
         <section className="wpo-hero-slider wpo-hero-style-1">
             <Swiper
-               
-                modules={[Navigation, A11y]}
+                modules={[Navigation, A11y, Parallax]}
                 spaceBetween={0}
                 slidesPerView={1}
                 loop={true}
                 speed={1800}
                 parallax={true}
-                navigation
+                navigation={true}
+                
+                /* FORCED INTERACTION ASSURANCE PARAMETERS */
+                observer={true}
+                observeParents={true}
+                style={{ width: "100%", height: "100vh" }} 
+                onSwiper={(swiper) => {
+                    setTimeout(() => {
+                        if (swiper && !swiper.destroyed) {
+                            swiper.update();
+                        }
+                    }, 150);
+                }}
             >
-                <SwiperSlide>
-                    <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero1})` }}>
+                {/* Slide 1 */}
+                <SwiperSlide style={{ width: "100%", height: "100%" }}>
+                    <div 
+                        className="slide-inner slide-bg-image" 
+                        style={{ backgroundImage: `url(${hero1})`, height: "100%", width: "100%" }}
+                    >
                         <div className="container-fluid">
                             <div className="slide-content">
-                                <div  className="slide-title">
+                                <div className="slide-title" data-swiper-parallax="-400">
                                     <h2>To <span>industry</span>, nothing is impossible.</h2>
                                 </div>
-                                <div  className="slide-title-sub">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, justo,
-                                        habitant.</p>
+                                <div className="slide-title-sub" data-swiper-parallax="-300">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, justo, habitant.</p>
                                 </div>
                                 <div className="clearfix"></div>
-                                <div className="slide-btns">
-                                    <Link onClick={ClickHandler} to="/about" className="theme-btn">Get Started</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                  
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero2})` }}>
-                        <div className="container-fluid">
-                            <div className="slide-content">
-                                <div  className="slide-title">
-                                    <h2>To <span>industry</span>, nothing is impossible.</h2>
-                                </div>
-                                <div className="slide-title-sub">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, justo,
-                                        habitant.</p>
-                                </div>
-                                <div className="clearfix"></div>
-                                <div  className="slide-btns">
+                                <div className="slide-btns" data-swiper-parallax="-200">
                                     <Link onClick={ClickHandler} to="/about" className="theme-btn">Get Started</Link>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
-                ...
+
+                {/* Slide 2 */}
+                <SwiperSlide style={{ width: "100%", height: "100%" }}>
+                    <div 
+                        className="slide-inner slide-bg-image" 
+                        style={{ backgroundImage: `url(${hero2})`, height: "100%", width: "100%" }}
+                    >
+                        <div className="container-fluid">
+                            <div className="slide-content">
+                                <div className="slide-title" data-swiper-parallax="-400">
+                                    <h2>To <span>industry</span>, nothing is impossible.</h2>
+                                </div>
+                                <div className="slide-title-sub" data-swiper-parallax="-300">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, justo, habitant.</p>
+                                </div>
+                                <div className="clearfix"></div>
+                                <div className="slide-btns" data-swiper-parallax="-200">
+                                    <Link onClick={ClickHandler} to="/about" className="theme-btn">Get Started</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
             </Swiper>
         </section>
-    )
-}
+    );
+};
 
 export default Hero;
-
-
